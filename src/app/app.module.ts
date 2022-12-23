d45703801d24c8b13dc1c14cf9110b448537e125
Serghei <pavlov.serghei@tutanota.com>
2022-12-23 12:17:55 +0100

    fixed images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SliderComponent } from './slider/slider.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { LanguageSelectComponent } from './header/language-select/language-select.component';
-import { SearchComponent } from './landing-page/search/search.component';
+import { HeaderComponent } from './areas/header/header.component';
+import { FooterComponent } from './areas/footer/footer.component';
+import { SliderComponent } from './areas/slider/slider.component';
+import { LandingPageComponent } from './areas/landing-page/landing-page.component';
+import { LanguageSelectComponent } from './areas/header/language-select/language-select.component';
+import { SearchComponent } from './areas/landing-page/search/search.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ImageTextComponent } from './image-text/image-text.component';
-import { ProductPresentationComponent } from './product-presentation/product-presentation.component';
-import { CounterComponent } from './counter/counter.component';
-import { ProgressComponent } from './progress/progress.component';
-import { StatsComponent } from './stats/stats.component';
+import { ImageTextComponent } from './areas/image-text/image-text.component';
+import { ProductPresentationComponent } from './areas/product-presentation/product-presentation.component';
+import { CounterComponent } from './areas/counter/counter.component';
+import { ProgressComponent } from './areas/progress/progress.component';
+import { StatsComponent } from './areas/stats/stats.component';
+import { WarningComponent } from './components/warning/warning.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { StatsComponent } from './stats/stats.component';
     CounterComponent,
     ProgressComponent,
     StatsComponent,
+    WarningComponent,
   ],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
